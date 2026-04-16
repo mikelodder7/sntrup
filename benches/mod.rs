a8909bc5edc0a3cc07ae6e06b3f8cd66b25d81a4
Mike Lodder <redmike7@gmail.com>
2026-04-16 03:18:12 -0600

    fix for CI
    Signed-off-by: Mike Lodder <redmike7@gmail.com>

--- benches/mod.rs
+++ benches/mod.rs
@@ -1,6 +1,6 @@
 #![allow(missing_docs)]
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 use sntrup::*;
 
 fn bench_keygen(c: &mut Criterion) {
